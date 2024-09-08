a7150452dc0e2b70a1d71266129d55d9b048f317
Lokesh P S <lokeshpspsl522@gmail.com>
2024-09-08 18:37:00 +0530

    changes for deployment

--- dating-app-backend/server.js
+++ dating-app-backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import Cors from "cors";
-
+import path from "path";
 import Cards from "./dbCards.js";
 const app = express();
 const port = process.env.PORT || 8001;
@@ -18,9 +18,9 @@ mongoose
     console.error("Error connecting to MongoDB:", error);
   });
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello This is Dating App");
-});
+// app.get("/", (req, res) => {
+//   res.status(200).send("Hello This is Dating App");
+// });
 app.post("/dating/cards", async (req, res) => {
   const dbcard = req.body;
 
@@ -39,5 +39,20 @@ app.get("/dating/cards", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+const __dirname1 = path.resolve();
+const projectDir = path.dirname(__dirname1);
+let env = "production";
+if (env === "production") {
+  app.use(express.static(path.join(projectDir, "/dating-app-frontend/build")));
 
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(projectDir, "dating-app-frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is Running Successfully");
+  });
+}
 app.listen(port, () => console.log(`Listening on localhost: ${port}`));
